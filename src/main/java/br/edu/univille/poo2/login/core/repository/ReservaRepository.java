698d72fc1d24e73e3880cf2003bd4b47c2796a2f
NikolasAndreskiRodrigues <128433982+NikolasAndreskiRodrigues@users.noreply.github.com>
2024-11-11 19:20:27 -0300

    fix

--- src/main/java/br/edu/univille/poo2/login/core/repository/ReservaRepository.java
+++ src/main/java/br/edu/univille/poo2/login/core/repository/ReservaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ReservaRepository extends JpaRepository<> {
-    Optional<User> findByUsernameAndActiveTrue(String username);
+public interface ReservaRepository {
+    ;
 }
