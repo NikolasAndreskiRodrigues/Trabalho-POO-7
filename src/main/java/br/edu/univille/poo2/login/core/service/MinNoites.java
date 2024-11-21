33ccbc396e98f38fe5cc8c8a9b7d14aab15a3aed
Mateus Medeiros dos Santos <106558452+Th3FIa5h@users.noreply.github.com>
2024-11-21 20:27:37 -0300

    Mais commit
    .

--- src/main/java/br/edu/univille/poo2/login/core/service/MinNoites.java
+++ src/main/java/br/edu/univille/poo2/login/core/service/MinNoites.java
@@ -1,9 +1,12 @@
 package br.edu.univille.poo2.login.core.service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Service
 public class MinNoites {
@@ -16,6 +19,7 @@ public class MinNoites {
     private static final int MIN_NIGHTS = 1;
 
     public MinNoites(LocalDate dataCheckIn, LocalDate dataCheckOut) {
+
         this.dataCheckIn = dataCheckIn;
         this.dataCheckOut = dataCheckOut;
     }
@@ -23,5 +27,14 @@ public class MinNoites {
     public boolean verificarDisponibilidade() {
         long numNoites = ChronoUnit.DAYS.between(dataCheckIn, dataCheckOut);
         return numNoites >= MIN_NIGHTS;
+    }
+    @Override
+    public MinNoites loadUserByUsername(String username) throws UsernameNotFoundException {
+        var opt = userRepository.findByUsernameAndActiveTrue(username);
+        if(opt.isEmpty()) {throw new UsernameNotFoundException(username);}
+        var user = opt.get();
+        var list = List.of(user.getRole());
+        System.out.println(user);
+        return new  org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(list));
     }
 }
