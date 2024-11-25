ac6e6ef927261d9b4ffde09a01ce62dd51e1bf17
NikolasAndreskiRodrigues <128433982+NikolasAndreskiRodrigues@users.noreply.github.com>
2024-11-24 22:57:03 -0300

    cadastro controller

---
+++ src/main/java/br/edu/univille/poo2/login/core/repository/AvaliacaoRepository.java
@@ -0,0 +1,8 @@
+package br.edu.univille.poo2.login.core.repository;
+
+import br.edu.univille.poo2.login.core.entity.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+}
+
